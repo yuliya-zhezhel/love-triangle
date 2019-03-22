c8abda6165be3363de25e9b922a0a344e38db9ba
yuliya-zhezhel <yuliya.zhezhel@gmail.com>
2019-03-22 11:35:07 +0300

    lovetriangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	var count = 0;
+	preferences.unshift(0);
+	var length = preferences.length;
+	for(var i = 1; i < length; i++){
+		var presentValue = preferences[i];
+		var nextValue = preferences[presentValue];
+		if(preferences[nextValue]==i) count+=1;
+		}
+		count=count/3;
+		return count-(count%1);
 };
